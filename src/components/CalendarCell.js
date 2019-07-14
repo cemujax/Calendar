45c6d38b5d1c07e9eb9d0f4bc2057b3e3aa7704d
cemujax <cemujax@gmail.com>
2019-07-14 17:51:51 +0900

    TST: CalendarCell alert event test

--- src/components/CalendarCell.js
+++ src/components/CalendarCell.js
@@ -2,13 +2,24 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 const CalendarCell = ({ day }) => {
+  const handleShowEvent = events => {
+    if (events.length > 0) {
+      const msg = events.reduce((acc, event) => acc + event + '\n', '');
+      alert(msg);
+    }
+  };
+
   const dayEvents = day.events || [];
   return (
     <DayCell data-testid="day-cell">
       <DayLabel today={day.isToday} data-testid="day-label">
         {day.shortDate}
       </DayLabel>
-      <DayContent data-testid="day-content">
+      <DayContent
+        onClick={() => handleShowEvent(dayEvents)}
+        isEvent={dayEvents}
+        data-testid="day-content"
+      >
         {dayEvents.map((event, index) => (
           <DayEvent key={index}>{event}</DayEvent>
         ))}
@@ -17,16 +28,35 @@ const CalendarCell = ({ day }) => {
   );
 };
 
-const DayCell = styled.div``;
+const DayCell = styled.div`
+  flex: 1;
+  max-width: calc(100% / 7);
+  text-align: center;
+  border: 1px solid #ccc;
+`;
 
 const DayLabel = styled.div`
+  border-bottom: 1px solid #ccc;
+
   /* 오늘 날짜 하이라이팅 */
   background: ${props => (props.today ? '#FA8072' : '#f7f9f9')};
   color: ${props => (props.today ? 'white' : 'black')};
 `;
 
-const DayContent = styled.div``;
+const DayContent = styled.div`
+  height: 100px;
+  max-height: 150px;
+  overflow: auto;
+  padding: 5px;
+  cursor: ${props => (props.isEvent.length > 0 ? 'pointer' : '')};
 
-const DayEvent = styled.div``;
+  :hover {
+    background: ${props => (props.isEvent.length > 0 ? '#f0f0f0' : '')};
+  }
+`;
+
+const DayEvent = styled.div`
+  font-size: 14px;
+`;
 
 export default CalendarCell;
--- src/components/test/CalendarCell.test.js
+++ src/components/test/CalendarCell.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { getDaysInMonth } from '../../utils/date';
 import moment from 'moment';
 
@@ -17,6 +17,7 @@ describe('<CalendarCell />', () => {
     },
   };
   const daysInToday = getDaysInMonth(moment());
+  const daysInMay = getDaysInMonth(moment('2019-05'));
 
   it('Today CalendarCell', () => {
     const today = daysInToday.find(
@@ -32,5 +33,53 @@ describe('<CalendarCell />', () => {
     // today 스타일 체크
     expect(DayLable).toHaveStyle(`background: ${cellStyle.today.background}`);
     expect(DayLable).toHaveStyle(`color: ${cellStyle.today.color}`);
+  });
+
+  // 이벤트 없음
+  it('2019-05-03 CalendarCell', () => {
+    global.alert = jest.fn();
+    const day0503 = daysInMay.find(day => day.date === '2019-05-03');
+    const { getByText, getByTestId } = render(<CalendarCell day={day0503} />);
+    const DayLable = getByText('3'); // shortDate
+
+    // today 유무 스타일 체크
+    expect(DayLable).toHaveStyle(`background: ${cellStyle.default.background}`);
+    expect(DayLable).toHaveStyle(`color: ${cellStyle.default.color}`);
+
+    const DayContent = getByTestId('day-content');
+    const DayEventNodes = DayContent.children;
+
+    // Event
+    expect(DayEventNodes.length).toBe(0); // 이벤트 갯수
+
+    // 클릭 및 alert 호출 테스트
+    fireEvent.click(DayContent);
+    expect(global.alert).toHaveBeenCalledTimes(0);
+  });
+
+  // 이벤트 2개 존재
+  it('2019-05-05 CalendarCell', () => {
+    global.alert = jest.fn();
+    const day0505 = daysInMay.find(day => day.date === '2019-05-05');
+    const { getByText, getByTestId } = render(<CalendarCell day={day0505} />);
+    const DayLable = getByText('5'); // shortDate
+
+    // today 유무 스타일 체크
+    expect(DayLable).toHaveStyle(`background: ${cellStyle.default.background}`);
+    expect(DayLable).toHaveStyle(`color: ${cellStyle.default.color}`);
+
+    const DayContent = getByTestId('day-content');
+    const DayEventNodes = DayContent.children;
+
+    // Event
+    expect(DayEventNodes.length).toBe(2); // 이벤트 갯수
+    const Event1 = getByText('어린이날');
+    const Event2 = getByText('친구와 약속');
+    expect(DayEventNodes[0]).toBe(Event1);
+    expect(DayEventNodes[1]).toBe(Event2);
+
+    // alert 호출 테스트
+    fireEvent.click(DayContent);
+    expect(global.alert).toHaveBeenCalledTimes(1);
   });
 });
