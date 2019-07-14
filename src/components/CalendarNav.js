1b265c559ca1528d8cb44112e056c8a245ef3769
cemujax <cemujax@gmail.com>
2019-07-14 15:56:43 +0900

    TST: 이전월, 다음월 동작 테스트

--- src/components/CalendarNav.js
+++ src/components/CalendarNav.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-const CalendarNav = ({ currentMonth }) => {
+const CalendarNav = ({ currentMonth, onPrev, onNext }) => {
   return (
     <Nav>
-      <NavButton>이전월</NavButton>
+      <NavButton onClick={onPrev}>이전월</NavButton>
       <CurrentMonth data-testid="current-month">{currentMonth}</CurrentMonth>
-      <NavButton>다음월</NavButton>
+      <NavButton onClick={onNext}>다음월</NavButton>
     </Nav>
   );
 };
--- src/components/test/CalendarNav.test.js
+++ src/components/test/CalendarNav.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import moment from 'moment';
 
 import CalendarNav from '../CalendarNav';
@@ -16,8 +16,49 @@ describe('<CalendarNav />', () => {
 
   it('corrects CurrentMonth value', () => {
     const currentMonth = moment().format(DATE_FORMAT);
-    console.log('currentMonth', currentMonth);
     const { getByTestId } = render(<CalendarNav currentMonth={currentMonth} />);
     expect(getByTestId('current-month').textContent).toBe(currentMonth); // 현재월 출력
+  });
+
+  it('calls onPrev and update props(currentMonth)', () => {
+    const onPrev = jest.fn();
+    const currentMonth = moment().format(DATE_FORMAT);
+    const prevMonth = moment()
+      .subtract(1, 'months')
+      .format(DATE_FORMAT);
+
+    const { getByText, getByTestId, rerender } = render(
+      <CalendarNav currentMonth={currentMonth} onPrev={onPrev} />,
+    );
+    const prevButton = getByText('이전월');
+
+    expect(getByTestId('current-month').textContent).toBe(currentMonth); // 현재월 출력
+
+    fireEvent.click(prevButton); // 이전월 버튼 클릭
+    expect(onPrev).toHaveBeenCalledTimes(1);
+
+    rerender(<CalendarNav currentMonth={prevMonth} onPrev={onPrev} />);
+    expect(getByTestId('current-month').textContent).toBe(prevMonth); // 이전월 출력
+  });
+
+  it('calls onNext and update props(currentMonth)', () => {
+    const onNext = jest.fn();
+    const currentMonth = moment().format(DATE_FORMAT);
+    const nextMonth = moment()
+      .add(1, 'months')
+      .format(DATE_FORMAT);
+
+    const { getByText, getByTestId, rerender } = render(
+      <CalendarNav currentMonth={currentMonth} onNext={onNext} />,
+    );
+    const nextButton = getByText('다음월');
+
+    expect(getByTestId('current-month').textContent).toBe(currentMonth); // 현재월 출력
+
+    fireEvent.click(nextButton); // 다음월 버튼 클릭
+    expect(onNext).toHaveBeenCalledTimes(1);
+
+    rerender(<CalendarNav currentMonth={nextMonth} onNext={onNext} />);
+    expect(getByTestId('current-month').textContent).toBe(nextMonth);
   });
 });
